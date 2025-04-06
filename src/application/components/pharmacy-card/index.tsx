e0fcfc37eb64cb96a4134f4f5f18aef21115baaf
DevViniciusAmaral <dev.viniciusamaral@gmail.com>
2025-04-06 14:02:19 -0300

    refactor: atualiazado componente PharmacyCard

--- src/application/components/pharmacy-card/index.tsx
+++ src/application/components/pharmacy-card/index.tsx
@@ -1,22 +1,27 @@
-import { Image, View } from "react-native";
+import { Image, TouchableOpacityProps, View } from "react-native";
 import { useStyles } from "react-native-unistyles";
 import { stylesheet } from "./styles";
 import { Text } from "../text";
+import { Button } from "../button";
 
-export const PharmacyCard = () => {
+export const PharmacyCard = ({ style, ...rest }: TouchableOpacityProps) => {
   const { styles } = useStyles(stylesheet);
 
   return (
-    <View style={styles.container}>
+    <Button {...rest} style={[styles.container, style]}>
       <Image style={styles.image} />
 
       <View>
-        <Text size={18} font="semibold">Ultra Farma</Text>
+        <Text size={18} font="semibold">
+          Ultra Farma
+        </Text>
         <Text style={styles.description}>Rua Dutra, Centro, São Paulo</Text>
         <View style={styles.distanceContent}>
-            <Text size={12} font="semibold" style={styles.distanceText}>1.2 km</Text>
+          <Text size={12} font="semibold" style={styles.distanceText}>
+            1.2 km
+          </Text>
         </View>
       </View>
-    </View>
+    </Button>
   );
 };
